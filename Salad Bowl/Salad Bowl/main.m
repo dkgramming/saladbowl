ffe6ff5f297348f8d1a46a038c8c04ed8dbc7982
David Kisluk <david.kisluk@gmail.com>
2015-02-15 16:40:30 -0500

    Initialize xcode project

---
+++ Salad Bowl/Salad Bowl/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Salad Bowl
+//
+//  Created by Cailin Li on 2/15/15.
+//  Copyright (c) 2015 Davelin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ Salad Bowl/Salad BowlTests/Salad_BowlTests.m
@@ -0,0 +1,40 @@
+//
+//  Salad_BowlTests.m
+//  Salad BowlTests
+//
+//  Created by Cailin Li on 2/15/15.
+//  Copyright (c) 2015 Davelin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface Salad_BowlTests : XCTestCase
+
+@end
+
+@implementation Salad_BowlTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
