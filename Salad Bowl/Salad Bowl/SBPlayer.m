50d7fb05def1d9521fdf6aec7ff042449877f8e8
David Kisluk <david.kisluk@gmail.com>
2015-02-15 16:56:25 -0500

    Add model class for Salad Bowl players

---
+++ Salad Bowl/Salad Bowl/SBPlayer.m
@@ -0,0 +1,13 @@
+//
+//  SBPlayer.m
+//  Salad Bowl
+//
+//  Created by Cailin Li on 2/15/15.
+//  Copyright (c) 2015 Davelin. All rights reserved.
+//
+
+#import "SBPlayer.h"
+
+@implementation SBPlayer
+
+@end
