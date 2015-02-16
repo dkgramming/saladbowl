383e3fd340de14ad4d7261a65661bcf2cff2b179
David Kisluk <david.kisluk@gmail.com>
2015-02-15 21:59:58 -0500

    Add model class for a Salad Bowl Team

---
+++ Salad Bowl/Salad Bowl/SBTeam.h
@@ -0,0 +1,13 @@
+//
+//  SBTeam.h
+//  Salad Bowl
+//
+//  Created by Cailin Li on 2/15/15.
+//  Copyright (c) 2015 Davelin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SBTeam : NSObject
+
+@end
